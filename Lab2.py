3889b11d340af654dfc2f212d42ac51d2010aed7
GeorgeTin <tintareanu.george@gmail.com>
2019-02-15 15:12:05 +0100

    Test

--- Lab2.py
+++ Lab2.py
@@ -1,13 +1,47 @@
 import numpy as np
-import random as rand
-import math as math
-from scipy.optimize import minimize
-import matplotlib.pyplot as plt
 
-def kernel(xVector,yVector):
-#linear kernel function for
-  scalar = np.dot(xVector, yVector)
-  return scalar
+# datapoints user for training and test
+x_datapoints = np.array([1,2,3])
+
+# TODO: find how to compute t_array
+t_array = np.array([1,2,3])
+
+matrix_p = np.matrix([[]])
+
+
+def objective(alpha_array):
+
+    sum_of_alpha = np.sum(alpha_array)
+    sum = 1/2 * compute_sum(alpha_array) - sum_of_alpha
+    return sum
+
+
+def pre_compute_matrix_p():
+
+    matrix_p = np.outer(t_array, t_array)
+
+    for i in range(t_array.size):
+        for j in range(t_array.size):
+            matrix_p[i][j] *= kernel(x_datapoints[i], x_datapoints[j])
+
+
+def compute_sum(alpha_array):
+    resulted_matrix = np.outer(alpha_array, alpha_array)
+
+    for i in range(len(alpha_array)):
+        for j in range(len(alpha_array[0])):
+            resulted_matrix[i][j] *= matrix_p[i][j]
+
+    # TODO: compute the sum of all the elements in the matrix resulted_matrix
+
+    return resulted_matrix
+
+
+def kernel(x_vector, y_vector):
+
+    # linear kernel function for
+    scalar = np.dot(x_vector, y_vector)
+    return scalar
 
 def zerofun(a,t):
 #implements equality constrain of function (10)
@@ -18,10 +52,8 @@ def zerofun(a,t):
   else:
       return False
 
-#minimize function call here
-#ret =minimize(objective,start,bounds=B,constrains=XC)
-#alpha = ret['x']
-#print(alpha)
+#pre-compute matrix P (call the function only once at the beginning)
+pre_compute_matrix_p()
 
 
 #test kernel function
